acf0b0d557e7af841a7c26c4f487a25e107a5f89
mukeshshar24@gmail.com <mukeshsharma24@gmail.com>
2017-03-07 20:23:53 +0530

    added exception handling to check_bucket.py

--- S3/check_bucket.py
+++ S3/check_bucket.py
@@ -1,8 +1,6 @@
 #!usr/bin/python
 #Fri Feb 10 22:27:42 IST 2017
-"""
-Pass the S3 buket name as command line argument to check its availbility in your S3
-"""
+
 import argparse
 import boto3
 
@@ -16,7 +14,7 @@ def bucket_check(bucket_name):
         bucket = s3.Bucket(bucket_name)
         print "\n\n\t Looking up the bucket ==>> ",bucket_name
         if s3.Bucket(bucket_name) in s3.buckets.all():
-            print "\n\n\tTrue! Bucket Exists ==>", bucket_name
+            print "\n\n\t True! Bucket Exists ==>", bucket_name
         else:
             print "\n\n\t False! Bucket Does not Exists == >",bucket_name
 
