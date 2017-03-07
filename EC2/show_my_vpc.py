e3cb05e2b28654be0c702d6d21fdc30615d209ef
mukeshshar24@gmail.com <mukeshsharma24@gmail.com>
2017-03-07 20:25:00 +0530

    Added EC2 dir

---
+++ EC2/show_my_vpc.py
@@ -0,0 +1,75 @@
+#!usr/bin/python
+
+#Mon Mar  6 22:20:52 IST 2017
+#Mukesh Sharma
+#Pass the vpcid to get detailed infromation about the vpc
+#Prerequisites plese have your IAM API Access Key and Secret Access Key set up in ~/.aws/credentials file
+
+import boto3
+import botocore
+import argparse
+
+def all_vpc_id():
+    try:
+        client = boto3.client('ec2')
+        vpc_details = client.describe_vpcs()
+
+        for vpc_id in vpc_details['Vpcs']:
+            print "VPC ID List \n"
+            print vpc_id['VpcId']
+
+        print "run --vpcid VPCID as optional argument to get all the vpc information"
+    except Exception as e:
+        print "Error =>",e
+
+
+
+def vpc_definition():
+    try:
+        #create the client session for ec2
+        client = boto3.client('ec2')
+        vpc_details = client.describe_vpcs(VpcIds=[args.vpcid])
+
+        print "Displaying the VPC information :"
+
+        for data in vpc_details['Vpcs']:
+            print "VPC ID =>",data['VpcId']
+            print "Tenancy =>",data['InstanceTenancy']
+            print "State   =>",data['State']
+            print "CIDR    =>",data['CidrBlock']
+            tags = str(data['Tags'])
+            tags_list = []
+            tags_list = tags
+            print "Vpc Tag Name =>",tags_list[13:-19]
+
+    except Exception as e:
+            print "ERROR => ",e
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="This script will show all your VPC information")
+    parser.add_argument("--vpcid",help="Enter the VpcID as the optional argument")
+
+    args = parser.parse_args()
+
+##### Execute the  methods
+
+    if args.vpcid:
+        vpc_definition()
+    else:
+        all_vpc_id()
+
+
+
+
+
+
+
+
+
+
+
+
+
--- S3/check_bucket.py
+++ S3/check_bucket.py
@@ -9,6 +9,7 @@ parser.add_argument('bucket',help="Enter the name of the S3 Bucket as a command 
 args = parser.parse_args()
 
 def bucket_check(bucket_name):
+    try:
 
         s3 = boto3.resource('s3')
         bucket = s3.Bucket(bucket_name)
@@ -17,8 +18,14 @@ def bucket_check(bucket_name):
             print "\n\n\t True! Bucket Exists ==>", bucket_name
         else:
             print "\n\n\t False! Bucket Does not Exists == >",bucket_name
+    except Exception as e:
+        print "Error =>",e
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script To check if the bucket exists")
+    parser.add_argument('bucket',help="Enter the name of the S3 Bucket as a command line argument")
+    args = parser.parse_args()
+
     bucket_check(args.bucket)
 
 
