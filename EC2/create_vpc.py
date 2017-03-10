f4dfe1b2f40213743c849cedd7a7180b2d7e5dcc
mukeshshar24@gmail.com <mukeshsharma24@gmail.com>
2017-03-10 11:54:46 +0530

    creates vpc,subnet,igw and attaches the igw to the vpc

--- EC2/create_vpc.py
+++ EC2/create_vpc.py
@@ -19,6 +19,7 @@ print "Connected to EC2"
 def vpc_create():
     try:
 
+        """ This function creates the VPC """
         print "Creating VPC"
         response_create_vpc = client.create_vpc(
             DryRun=dry_run_false,
@@ -32,12 +33,38 @@ def vpc_create():
         print vpcid
         #print vpc_id['VpcId']
 
+        """ This function creates the Subnet"""
         response_create_subnet = client.create_subnet(
                 DryRun=dry_run_false,
                 CidrBlock =subnet_cidr,
                 VpcId=vpcid
                 )
-        print "Created subnet ",response_create_subnet
+        print "Created subnet "
+        subnet_id=response_create_subnet['Subnet']
+        subnetid = subnet_id['SubnetId']
+        print subnetid
+
+        """THis function creates the internet gateway"""
+        response_create_igw =client.create_internet_gateway(
+                DryRun=dry_run_false
+                )
+        print "Created the Internet Gateway"
+        igw_id=response_create_igw['InternetGateway']
+        igwid = igw_id['InternetGatewayId']
+        print igwid
+
+        #attach the internetgateway to the vpc
+        response_atttach_igw = client.attach_internet_gateway(
+                DryRun=dry_run_false,
+                InternetGatewayId=igwid,
+                VpcId=vpcid
+                )
+        print "Attached IGW " + igwid + " to the VPC " + vpcid
+
+
+
+
+
 
     except Exception as e:
         print "ERROR=>\n" ,e
